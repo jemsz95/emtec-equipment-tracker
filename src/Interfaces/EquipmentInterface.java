1efb0069eeebfc3fbfae4bfe17bf3476795cf7af
Javier Meza <javenmeza@gmail.com>
2016-11-22 12:55:43 -0600

    Added moment and started templating equipemnt card

--- src/Interfaces/EquipmentInterface.java
+++ src/Interfaces/EquipmentInterface.java
@@ -5,10 +5,17 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.util.*;
 
+import Entities.Equipment;
+
 public class EquipmentInterface extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    Equipment e = new Equipment();
+
+    //e.registerEquipment("Camilla");
+
     PrintWriter out = response.getWriter();
-    out.println("Hello!");
+    //out.println(e.searchEquipment("Camilla"));
+    out.println("Hola!");
   }
 
 }
--- web/app/equipment/equipment-card.component.ts
+++ web/app/equipment/equipment-card.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Equipment } from './equipment';
 
 @Component({
   selector: 'equipment-card',
   templateUrl: 'app/equipment/equipment-card.component.html'
 })
-export class EquipmentCardComponent {  }
+export class EquipmentCardComponent {  
+  @Input() equipment: Equipment;
+  lastLocation: MovementRecord;
+}
